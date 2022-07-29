acca25c20a19077baf85768e287c5989dcba6f7d
Arthur Neuman <arthurbneuman@gmail.com>
2022-07-28 21:56:52 -0400

    Revamp debugging

--- src/stories/FluentSelectionMixin.stories.jsx
+++ src/stories/FluentSelectionMixin.stories.jsx
@@ -1,4 +1,8 @@
-import React, { useEffect } from 'react'
+import {
+  React,
+  useRef,
+  useEffect
+} from 'react'
 
 import {
   FluentSelectionMixin
@@ -6,14 +10,33 @@ import {
 
 import './debug.css'
 
-function positionDebug (e) {
-  const first = document.getElementById('fluentdebugfirst')
-  const second = document.getElementById('fluentdebugsecond')
+function positionDebugTouches (e) {
+  const touches = []
+  for (const touch of e.touches) touches[touch.identifier] = touch
 
-  first.style.left = e.touches[0].clientX + 'px'
-  first.style.top = e.touches[0].clientY + 'px'
-  second.style.left = (e.touches[1]?.clientX || 0) + 'px'
-  second.style.top = (e.touches[1]?.clientY || 0) + 'px'
+  const touchPixels = document.getElementsByClassName('fluent debug touch')
+
+  for (let p = 0; p < touchPixels.length; p++) {
+    touchPixels[p].style.left = (touches[p]?.clientX || 0) + 'px'
+    touchPixels[p].style.top = (touches[p]?.clientY || 0) + 'px'
+  }
+}
+
+function positionDebugRange (ref, e) {
+  const rects = ref.current.originRange.getClientRects()
+  const rect = [
+    rects[0]?.left + 6,
+    rects[0]?.top,
+    rects[rects.length - 1]?.right - 6,
+    rects[rects.length - 1]?.top
+  ]
+
+  const rangeHandles = document.getElementsByClassName('fluent debug range')
+
+  for (let r = 0; r < rangeHandles.length; r++) {
+    rangeHandles[r].style.left = rect[r * 2] + 'px'
+    rangeHandles[r].style.top = rect[(r * 2) + 1] + 'px'
+  }
 }
 
 export default {
@@ -39,26 +62,34 @@ export default {
 }
 
 export const Default = (args) => {
+  const ref = useRef(null)
+  const boundPositionDebugRange = useRef(positionDebugRange.bind(this, ref))
+
   useEffect(() => {
     if (args.debug) {
-      document.addEventListener('touchstart', positionDebug)
-      document.addEventListener('touchmove', positionDebug)
+      document.addEventListener('touchstart', positionDebugTouches)
+      document.addEventListener('touchmove', positionDebugTouches)
+      document.addEventListener('touchmove', boundPositionDebugRange.current)
     } else {
-      document.removeEventListener('touchstart', positionDebug)
-      document.removeEventListener('touchmove', positionDebug)
+      document.removeEventListener('touchstart', positionDebugTouches)
+      document.removeEventListener('touchmove', positionDebugTouches)
+      document.removeEventListener('touchmove', boundPositionDebugRange.current)
     }
-  })
+  }, [args.debug])
 
   return (
     <div>
       <span id='span1'>This is some sample text!</span>
-      <FluentSelectionMixin {...args}/>
+      <FluentSelectionMixin {...args} ref={ref}/>
 
       {args.debug
         ? (
           <>
-            <div className='fluent debug' id='fluentdebugfirst'/>
-            <div className='fluent debug' id='fluentdebugsecond'/>
+            <div className='fluent debug touch first'/>
+            <div className='fluent debug touch second'/>
+
+            <div className='fluent debug range first'/>
+            <div className='fluent debug range second'/>
           </>
           )
         : null}
