d432b58f2c5e10310e7648c5848e9cb4420b05ae
Arthur Neuman <arthurbneuman@gmail.com>
2022-08-21 20:18:32 -0400

    Use local version of PostCSS in preparation of SB7 migration

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,7 @@ module.exports = {
     '../src/**/*.stories.@(js|jsx|ts|tsx)'
   ],
   addons: [
+    '@storybook/addon-postcss',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-console'
